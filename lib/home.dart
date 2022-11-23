6c2ab63b9b62909725e8f439b21b62d06e1ab85f
tauhid <ranguni47@gmail.com>
2022-11-23 12:18:17 +0530

    day3

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,17 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("party hard"),
       ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(100, 100, 100, 100),
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/img1.jpg"),
+                  fit: BoxFit.cover)),
+        ),
+      ),
     );
   }
 }
